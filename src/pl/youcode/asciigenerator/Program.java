e89c451384123aa8a23f75d48393cff7b34a1491
= <=>
2012-11-07 21:54:28 +0100

    initial commit

---
+++ src/pl/youcode/asciigenerator/Program.java
@@ -0,0 +1,50 @@
+package pl.youcode.asciigenerator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("#---------------------------#");
+		System.out.println("# Image to ASCII conventer   ");
+		System.out.println("#   by: Thelleo              ");
+		System.out.println("#   http://youcode.pl        ");
+		System.out.println("#---------------------------#");
+		boolean run = true;
+		do
+		{
+			System.out.println("");
+			System.out.println("Type path to file which you want to convert.");
+			System.out.println("If you want to exit type: QUIT");
+			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+		    try
+			{
+				String filename = bufferRead.readLine();
+				if(filename == "QUIT")
+				{
+					run = false;
+					break;
+				}
+				try
+				{
+					Image2ASCII art = new Image2ASCII(new File(filename));
+					art.save();
+				} 
+				catch (IOException e)
+				{
+					System.out.println(String.format("Error: %s", e.getLocalizedMessage()));
+				}
+			} 
+		    catch (IOException e)
+			{
+				System.out.println(String.format("Error: %s", e.getLocalizedMessage()));
+			}
+		} while(run);
+	}
+
+}
