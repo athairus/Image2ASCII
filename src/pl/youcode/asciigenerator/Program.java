30da9749955c1921d83633d251ad35a345fa320b
whiplash <whiplash000@gmail.com>
2013-02-11 10:07:24 -0500

    Changed source code format to my own style

--- src/pl/youcode/asciigenerator/Program.java
+++ src/pl/youcode/asciigenerator/Program.java
@@ -5,46 +5,39 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Program
-{
+public class Program {
 
-	public static void main(String[] args)
-	{
-		System.out.println("#---------------------------#");
-		System.out.println("# Image to ASCII conventer   ");
-		System.out.println("#   by: Thelleo              ");
-		System.out.println("#   http://youcode.pl        ");
-		System.out.println("#---------------------------#");
+	public static void main( String[] args ) {
+		System.out.println( "#---------------------------#" );
+		System.out.println( "# Image to ASCII conventer   " );
+		System.out.println( "#   by: Thelleo              " );
+		System.out.println( "#   http://youcode.pl        " );
+		System.out.println( "#---------------------------#" );
 		boolean run = true;
-		do
-		{
-			System.out.println("");
-			System.out.println("Type path to file which you want to convert.");
-			System.out.println("If you want to exit type: QUIT");
-			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-		    try
-			{
+		do {
+			System.out.println( "" );
+			System.out.println( "Type path to file which you want to convert." );
+			System.out.println( "If you want to exit type: QUIT" );
+			BufferedReader bufferRead = new BufferedReader(
+					new InputStreamReader( System.in ) );
+			try {
 				String filename = bufferRead.readLine();
-				if(filename == "QUIT")
-				{
+				if( filename.equals( "QUIT" ) ) {
 					run = false;
 					break;
 				}
-				try
-				{
-					Image2ASCII art = new Image2ASCII(new File(filename));
+				try {
+					Image2ASCII art = new Image2ASCII( new File( filename ) );
 					art.save();
-				} 
-				catch (IOException e)
-				{
-					System.out.println(String.format("Error: %s", e.getLocalizedMessage()));
+				} catch ( IOException e ) {
+					System.out.println( String.format( "Error: %s",
+							e.getLocalizedMessage() ) );
 				}
-			} 
-		    catch (IOException e)
-			{
-				System.out.println(String.format("Error: %s", e.getLocalizedMessage()));
+			} catch ( IOException e ) {
+				System.out.println( String.format( "Error: %s",
+						e.getLocalizedMessage() ) );
 			}
-		} while(run);
+		} while ( run );
 	}
 
 }
